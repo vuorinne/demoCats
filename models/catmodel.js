8360d0fde34a9f6019793c4d628e85aef186e5ba
Jari Vuorinen <vuorijar@gmail.com>
2018-07-13 12:44:47 +0300

    catmodel owner ref

--- models/catmodel.js
+++ models/catmodel.js
@@ -8,26 +8,6 @@ var Owner = require('../models/ownermodel');
 //
 var Schema = mongoose.Schema;
 
-/*
-This ownerschema is not related any way to ownermodel.js's schema. 
-This only creates dummy owner to cat with different ID each time you use same name for an owner.
-Better solution has to be made. 
-Remember that this works only WHEN YOU CREATE and VIEW THE DATA for cat.
-For the time beign we use this temporarily until the better solution is implemented.
-*/
-var ownerSchema = new Schema(
-  {
-    fname: {
-      type: String
-    },
-    lname: {
-      type: String
-    },
-    city: {
-      type: String
-    }
-  });
-
 // Create schema for collection
 var catSchema = new Schema(
   {
@@ -42,8 +22,7 @@ var catSchema = new Schema(
       max: [25, 'Max age is 25'],
       required: [true, 'No age?']
     },
-    owner: [ownerSchema]
-    //owner : { type: Schema.Types.ObjectId, ref: 'Owner' }
+    owner : { type: mongoose.Schema.Types.ObjectId, ref: 'Owner' }
   },
   {collection: "catcollection", versionKey: false}
 );
